0ad8103fbbe11880650162d3f702f6c5993f3f57
Ivan Florencio <ivanflorencio@hotmail.com>
2022-10-12 19:03:28 -0300

    NE-27 / NE-26 - Troca de Nome e Customização de Layout

--- src/app/users-modules/shared/menus.ts
+++ src/app/users-modules/shared/menus.ts
@@ -1,13 +1,63 @@
-export const MeuCadastro = {text: 'Meu Cadastro', icon: 'ta-user-o', path: `/meu-cadastro`};
-export const GerenciarUsers = {text: 'Gerenciar Usuários', icon: 'ta-group', path: `/gerenciar-usuarios`};
+export const MeuCadastro = {
+  text: "Meu Cadastro",
+  icon: "ta-user-o",
+  path: `/meu-cadastro`,
+};
+export const GerenciarUsers = {
+  text: "Gerenciar Usuários",
+  icon: "ta-group",
+  path: `/gerenciar-usuarios`,
+};
 
-
-export const Demandas = {text: 'Gestão de Demandas', icon: 'ta-projeto', path: `/demandas`};
-export const Captacoes = {text: 'Projetos - Captação de Propostas', icon: 'ta-extrato', path: `/captacoes`};
-export const Selecao = {text: 'Projetos - Priorização e Seleção', icon: 'ta-filtro-escuro', path: `/selecao`};
-export const Refinamento = {text: 'Projetos - Refinamento', icon: 'ta-lupa-acoes', path: `/refinamento`};
-export const Riscos = {text: 'Projetos - Identificação e medição de riscos ', icon: 'ta-alert', path: `/identificacao-riscos`};
-export const Aprovacao = {text: 'Projetos - Aprovação e Formalização ', icon: 'ta-file-check', path: `/formalizacao`};
-export const Execucao = {text: 'Projetos - Em execução', icon: 'ta-engrenagem-chave-escura1', path: `/projetos/em-execucao`};
-export const Finalizacao = {text: 'Projetos - Em finalização', icon: 'ta-ficha', path: `/projetos/em-finalizacao`};
-export const Configuracao = {text: 'Configurações do Sistema', icon: 'ta-gear', path: `/configuracoes`};
+export const Demandas = {
+  text: "Gestão de Demandas",
+  icon: "ta-projeto",
+  path: `/demandas`,
+};
+export const Captacoes = {
+  subtitle: "Projetos",
+  text: "Captação de Propostas",
+  icon: "ta-extrato",
+  path: `/captacoes`,
+};
+export const Selecao = {
+  subtitle: "Projetos",
+  text: "Priorização e Seleção",
+  icon: "ta-filtro-escuro",
+  path: `/selecao`,
+};
+export const Refinamento = {
+  subtitle: "Projetos",
+  text: "Refinamento",
+  icon: "ta-lupa-acoes",
+  path: `/refinamento`,
+};
+export const Riscos = {
+  subtitle: "Projetos",
+  text: "Identificação e medição de riscos ",
+  icon: "ta-alert",
+  path: `/identificacao-riscos`,
+};
+export const Aprovacao = {
+  subtitle: "Projetos",
+  text: "Aprovação e Formalização ",
+  icon: "ta-file-check",
+  path: `/formalizacao`,
+};
+export const Execucao = {
+  subtitle: "Projetos",
+  text: "Em execução",
+  icon: "ta-engrenagem-chave-escura1",
+  path: `/projetos/em-execucao`,
+};
+export const Finalizacao = {
+  subtitle: "Projetos",
+  text: "Em finalização",
+  icon: "ta-ficha",
+  path: `/projetos/em-finalizacao`,
+};
+export const Configuracao = {
+  text: "Configurações do Sistema",
+  icon: "ta-gear",
+  path: `/configuracoes`,
+};
