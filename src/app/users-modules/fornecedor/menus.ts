6c17a5fc987e8cd2690ad1bc138d7fc0a53f58dd
Ivan Florencio <ivanflorencio@hotmail.com>
2022-10-13 13:18:25 -0300

    NE-28: adicionar campo hora

--- src/app/users-modules/fornecedor/menus.ts
+++ src/app/users-modules/fornecedor/menus.ts
@@ -1,56 +1,48 @@
-import {HEADER_MENU, SIDEBAR_MENU} from '@app/commons';
+/* eslint-disable @typescript-eslint/naming-convention */
+import { HEADER_MENU, SIDEBAR_MENU } from '@app/commons';
 import {
-  MenuItemConsultarDados,
-  MenuItemRefpAprovado,
-  MenuItemRefpNovo,
-  MenuItemRefpPendente,
-  MenuItemRefpReprovado,
-  MenuItemRelatorioEtapa,
-  MenuItemRelatorioFinal,
-  MenuItemResultadosApoio,
-  MenuItemResultadosCapacitacao,
-  MenuItemResultadosCientifico, MenuItemResultadosEconomicos, MenuItemResultadosIntelectual, MenuItemResultadosSocioAmbientais,
-  PROJETO_EXECUCAO_MENU,
-  PROJETO_FINALIZADO_MENU
-} from '@app/pages/projetos/projeto/projeto';
-import {Execucao, MeuCadastro, Refinamento} from '@app/users-modules/shared/menus';
-
-export const SidebarMenu = {
-  provide: SIDEBAR_MENU,
-  useValue: [
-    {text: 'Projetos - Captação de Propostas', icon: 'ta-extrato', path: `/propostas`},
-    Refinamento,
-    Execucao
-  ]
-};
-
-export const HeaderMenu = {
-  provide: HEADER_MENU,
-  useValue: [
-    MeuCadastro
-  ]
-};
-
-export const ProjetoExecucaoMenu = {
-  provide: PROJETO_EXECUCAO_MENU,
-  useValue: [
+    MenuItemConsultarDados,
+    MenuItemRefpAprovado,
     MenuItemRefpNovo,
     MenuItemRefpPendente,
     MenuItemRefpReprovado,
-    MenuItemRefpAprovado,
-    MenuItemConsultarDados
-  ]
-};
-export const ProjetoFinalizadoMenu = {
-  provide: PROJETO_FINALIZADO_MENU,
-  useValue: [
-    MenuItemRelatorioFinal,
     MenuItemRelatorioEtapa,
+    MenuItemRelatorioFinal,
+    MenuItemResultadosApoio,
     MenuItemResultadosCapacitacao,
     MenuItemResultadosCientifico,
-    MenuItemResultadosApoio,
+    MenuItemResultadosEconomicos,
     MenuItemResultadosIntelectual,
     MenuItemResultadosSocioAmbientais,
-    MenuItemResultadosEconomicos
-  ]
+    PROJETO_EXECUCAO_MENU,
+    PROJETO_FINALIZADO_MENU,
+} from '@app/pages/projetos/projeto/projeto';
+import { Execucao, MeuCadastro, Refinamento } from '@app/users-modules/shared/menus';
+
+export const SidebarMenu = {
+    provide: SIDEBAR_MENU,
+    useValue: [{ subtitle: 'Projetos', text: 'Captação de Propostas', icon: 'ta-extrato', path: `/propostas` }, Refinamento, Execucao],
+};
+
+export const HeaderMenu = {
+    provide: HEADER_MENU,
+    useValue: [MeuCadastro],
+};
+
+export const ProjetoExecucaoMenu = {
+    provide: PROJETO_EXECUCAO_MENU,
+    useValue: [MenuItemRefpNovo, MenuItemRefpPendente, MenuItemRefpReprovado, MenuItemRefpAprovado, MenuItemConsultarDados],
+};
+export const ProjetoFinalizadoMenu = {
+    provide: PROJETO_FINALIZADO_MENU,
+    useValue: [
+        MenuItemRelatorioFinal,
+        MenuItemRelatorioEtapa,
+        MenuItemResultadosCapacitacao,
+        MenuItemResultadosCientifico,
+        MenuItemResultadosApoio,
+        MenuItemResultadosIntelectual,
+        MenuItemResultadosSocioAmbientais,
+        MenuItemResultadosEconomicos,
+    ],
 };
--- src/app/users-modules/shared/menus.ts
+++ src/app/users-modules/shared/menus.ts
@@ -1,70 +1,71 @@
+/* eslint-disable @typescript-eslint/naming-convention */
 export const MeuCadastro = {
-  text: "Meu Cadastro",
-  icon: "ta-user-o",
-  path: `/meu-cadastro`,
+    text: 'Meu Cadastro',
+    icon: 'ta-user-o',
+    path: `/meu-cadastro`,
 };
 export const GerenciarUsers = {
-  text: "Gerenciar Usuários",
-  icon: "ta-group",
-  path: `/gerenciar-usuarios`,
+    text: 'Gerenciar Usuários',
+    icon: 'ta-group',
+    path: `/gerenciar-usuarios`,
 };
 
 export const Demandas = {
-  text: "Gestão de Demandas",
-  icon: "ta-projeto",
-  path: `/demandas`,
+    text: 'Gestão de Demandas',
+    icon: 'ta-projeto',
+    path: `/demandas`,
 };
 export const Captacoes = {
-  subtitle: "Projetos",
-  text: "Captação de Propostas",
-  icon: "ta-extrato",
-  path: `/captacoes`,
+    subtitle: 'Projetos',
+    text: 'Captação de Propostas',
+    icon: 'ta-extrato',
+    path: `/captacoes`,
 };
 export const Selecao = {
-  subtitle: "Projetos",
-  text: "Priorização e Seleção",
-  icon: "ta-filtro-escuro",
-  path: `/selecao`,
+    subtitle: 'Projetos',
+    text: 'Priorização e Seleção',
+    icon: 'ta-filtro-escuro',
+    path: `/selecao`,
 };
 export const Refinamento = {
-  subtitle: "Projetos",
-  text: "Refinamento",
-  icon: "ta-lupa-acoes",
-  path: `/refinamento`,
+    subtitle: 'Projetos',
+    text: 'Refinamento',
+    icon: 'ta-lupa-acoes',
+    path: `/refinamento`,
 };
 export const Riscos = {
-  subtitle: "Projetos",
-  text: "Identificação e medição de riscos ",
-  icon: "ta-alert",
-  path: `/identificacao-riscos`,
+    subtitle: 'Projetos',
+    text: 'Identificação e medição de riscos ',
+    icon: 'ta-alert',
+    path: `/identificacao-riscos`,
 };
 export const Aprovacao = {
-  subtitle: "Projetos",
-  text: "Aprovação e Formalização ",
-  icon: "ta-file-check",
-  path: `/formalizacao`,
+    subtitle: 'Projetos',
+    text: 'Aprovação e Formalização ',
+    icon: 'ta-file-check',
+    path: `/formalizacao`,
 };
 export const Execucao = {
-  subtitle: "Projetos",
-  text: "Em execução",
-  icon: "ta-engrenagem-chave-escura1",
-  path: `/projetos/em-execucao`,
+    subtitle: 'Projetos',
+    text: 'Em execução',
+    icon: 'ta-engrenagem-chave-escura1',
+    path: `/projetos/em-execucao`,
 };
 export const Finalizacao = {
-  subtitle: "Projetos",
-  text: "Em finalização",
-  icon: "ta-ficha",
-  path: `/projetos/em-finalizacao`,
+    subtitle: 'Projetos',
+    text: 'Em finalização',
+    icon: 'ta-ficha',
+    path: `/projetos/em-finalizacao`,
 };
 
 export const GestaoPeD = {
-  text: "Gestão P&D",
-  icon: "ta-chart",
-  path: `/gestao-ped`,
+    text: 'Gestão P&D',
+    icon: 'ta-chart',
+    path: `/gestao-ped`,
 };
 
 export const Configuracao = {
-  text: "Configurações do Sistema",
-  icon: "ta-gear",
-  path: `/configuracoes`,
+    text: 'Configurações do Sistema',
+    icon: 'ta-gear',
+    path: `/configuracoes`,
 };
