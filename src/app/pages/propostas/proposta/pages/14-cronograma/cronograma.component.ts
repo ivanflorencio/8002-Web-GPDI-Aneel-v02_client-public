90abe6411c1707548e3ebb2a67e5dd72b850e879
Ivan Florencio <ivanflorencio@hotmail.com>
2022-10-20 01:04:53 -0300

    NE-16: remove mock e conecta cronograma com back

--- src/app/pages/propostas/proposta/pages/14-cronograma/cronograma.component.ts
+++ src/app/pages/propostas/proposta/pages/14-cronograma/cronograma.component.ts
@@ -4,12 +4,7 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { PropostaComponent } from '@app/pages/propostas/proposta/proposta.component';
 import { BehaviorSubject } from 'rxjs';
 import { async } from 'rxjs/internal/scheduler/async';
-
-const MOCK_QTD_MESES = 60;
-const MOCK_QTD_EMPRESAS = 8;
-const MOCK_QTD_ETAPAS = 10;
-const MOCK_INICIO_ANO = 2022;
-const MOCK_INICIO_MES = 8;
+import { PropostasService } from '../../services/propostas.service';
 @Component({
     selector: 'app-cronograma',
     templateUrl: './cronograma.component.html',
@@ -17,29 +12,23 @@ const MOCK_INICIO_MES = 8;
 })
 export class CronogramaComponent implements OnInit {
     loading = false;
-
     canEdit: boolean;
+    proposta: any;
     cronograma: any;
-    fixedHeader = false;
-    scrollOffset = 215;
-    tableWidth = 0;
 
-    constructor(protected router: Router, protected route: ActivatedRoute, protected parent: PropostaComponent) {}
+    constructor(
+        protected router: Router,
+        protected route: ActivatedRoute,
+        protected parent: PropostaComponent,
+        protected service: PropostasService
+    ) {}
 
-    @HostListener('window:scroll')
-    onWindowScroll() {
-        this.fixedHeader = (window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0) > this.scrollOffset;
-    }
-
-    async ngAfterViewInit() {
-        this.tableWidth = document.querySelector('.cronograma').clientWidth;
-    }
-
-    getMesesEtapas(anoInicio: number, mesInicio: number, qtdMeses: number) {
+    getMesesEtapas(anoInicio: number, mesInicio: number, numeroMeses: number) {
+        console.log({ anoInicio, mesInicio, numeroMeses });
         const meses = [];
         let somaAno = 0;
         let somaMes = mesInicio;
-        for (let index = 0; index < qtdMeses; index++) {
+        for (let index = 0; index < numeroMeses; index++) {
             meses.push({
                 calendario: somaMes,
                 corrido: index + 1,
@@ -51,61 +40,10 @@ export class CronogramaComponent implements OnInit {
         return meses;
     }
 
-    getEtapas() {
-        const etapas = [];
-        const tamanho = Math.floor(MOCK_QTD_MESES / MOCK_QTD_ETAPAS);
-        for (let index = 0; index < MOCK_QTD_ETAPAS; index++) {
-            etapas.push({
-                numero: index + 1,
-                etapa: this._getNomeMock(),
-                meses: Array.from(Array(tamanho).keys()).map((i) => i + 1 + tamanho * index),
-                aberto: false,
-                produto: this._getNomeMock(),
-            });
-        }
-        return etapas;
-    }
-
-    _getDesembolsosMock() {
-        const desembolsos = [];
-        for (let index = 0; index < MOCK_QTD_MESES; index++) {
-            desembolsos.push(20000 + Math.floor(Math.random() * 100000));
-        }
-        return desembolsos;
-    }
-
-    _getNomeMock() {
-        const nomes = [
-            'Lorem ipsum dolor sit amet, consectetur',
-            'Adipiscing Elit sed do eiusmod tempor incididunt',
-            'Labore Magna Et dolore magna aliqua',
-            'Enim Ad minim veniam, quis nostrud exercitation',
-            'Ullamco laboris nisi ut aliquip ex ea commodo consequat',
-            'Duis Aute irure dolor in reprehenderit in voluptate ',
-            'Velit Pariatur cillum dolore eu fugiat nulla pariatur',
-            'Excepteur Sint occaecat cupidatat non proident',
-            'Sunt Mollit in culpa qui officia deserunt mollit anim id est laborum',
-        ];
-        return nomes[Math.floor(Math.random() * nomes.length)];
-    }
-
-    getEmpresas() {
-        const desembolso = this._getDesembolsosMock();
-        const empresas = [];
-        for (let index = 0; index < MOCK_QTD_EMPRESAS; index++) {
-            const nome = this._getNomeMock().split(' ');
-            empresas.push({ nome: `${nome[0]} ${nome[1]} Incorporações`, desembolso, total: desembolso.reduce((a, b) => a + b, 0) });
-        }
-        return empresas;
-    }
-
-    async ngOnInit() {
-        const mesesEtapas = this.getMesesEtapas(MOCK_INICIO_ANO, MOCK_INICIO_MES, MOCK_QTD_MESES);
-        const etapas = this.getEtapas();
-        const empresas = this.getEmpresas();
-
-        console.log(etapas);
-
+    carregarCronograma(infoCronograma) {
+        const mesesEtapas = this.getMesesEtapas(infoCronograma.inicio.ano, infoCronograma.inicio.mes, infoCronograma.inicio.numeroMeses);
+        const etapas = infoCronograma.etapas;
+        const empresas = infoCronograma.empresas;
         const anos = Array.from(new Set(mesesEtapas.map((m) => m.ano)));
 
         const meses = anos.reduce((acc, curr) => {
@@ -148,11 +86,39 @@ export class CronogramaComponent implements OnInit {
         };
     }
 
+    carregarDetalheEtapa(numero) {
+        this.service.getDetalheEtapaCronograma(this.proposta.guid, numero).then((result) => {
+            this.cronograma = {
+                ...this.cronograma,
+                etapas: this.cronograma.etapas.map((etapa) => {
+                    if (etapa.numero === numero) {
+                        return { ...etapa, detalhe: result };
+                    }
+                    return etapa;
+                }),
+            };
+        });
+    }
+
+    async ngOnInit() {
+        this.service.proposta.subscribe((proposta) => {
+            this.proposta = proposta;
+        });
+        this.route.data.subscribe((data) => {
+            if (data && data.data) {
+                this.carregarCronograma(data.data);
+            }
+        });
+    }
+
     toggle(n) {
         this.cronograma = {
             ...this.cronograma,
             etapas: this.cronograma.etapas.map((etapa) => {
                 if (etapa.numero === n) {
+                    if (!etapa.aberto && !etapa.detalhe) {
+                        this.carregarDetalheEtapa(etapa.numero);
+                    }
                     return { ...etapa, aberto: !etapa.aberto };
                 }
                 return etapa;
