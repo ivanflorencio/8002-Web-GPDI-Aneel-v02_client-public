9f319dd20d77f64d33b549bcbcec7cba5297fa76
Ivan Florencio <ivanflorencio@hotmail.com>
2022-10-26 13:44:37 -0300

    NE-10: Alterar o limite de caracteres de 1000 para livre na tela de plano de trabalho; (Tela plano de trabalho)

--- src/app/core/components/forms/textarea/textarea.component.ts
+++ src/app/core/components/forms/textarea/textarea.component.ts
@@ -1,85 +1,84 @@
-import {Component, Input, forwardRef, ElementRef} from '@angular/core';
-import {ControlValueAccessor, NG_VALUE_ACCESSOR} from '@angular/forms';
-
+import { Component, Input, forwardRef, ElementRef } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 
 @Component({
-  selector: 'app-textarea',
-  template: `
-    <div>
-        <textarea [class]="className"
-                  [maxlength]="maxlength"
-                  [name]="name"
-                  [placeholder]="placeholder"
-                  [cols]="cols"
-                  [id]="id"
-                  [rows]="rows"
-                  [disabled]="disabled"
-                  [(ngModel)]="value"></textarea>
-      <div *ngIf="maxlength > 0" class="text-right fs-12 font-italic p-1">{{charsLeft}} caracteres sobrando</div>
-    </div>
-  `,
-  styles: [],
-  providers: [
-    {
-      provide: NG_VALUE_ACCESSOR,
-      useExisting: forwardRef(() => TextareaComponent),
-      multi: true
-    }
-  ],
+    selector: 'app-textarea',
+    template: `
+        <div>
+            <textarea
+                [class]="className"
+                [maxlength]="maxlength"
+                [name]="name"
+                [placeholder]="placeholder"
+                [cols]="cols"
+                [id]="id"
+                [rows]="rows"
+                [disabled]="disabled"
+                [(ngModel)]="value"></textarea>
+            <div *ngIf="maxlength > 0" class="text-right fs-12 font-italic p-1">{{ charsLeft }} caracteres sobrando</div>
+        </div>
+    `,
+    styles: [],
+    providers: [
+        {
+            provide: NG_VALUE_ACCESSOR,
+            useExisting: forwardRef(() => TextareaComponent),
+            multi: true,
+        },
+    ],
 })
 export class TextareaComponent implements ControlValueAccessor {
+    @Input() name = '';
+    @Input() placeholder = '';
+    @Input() cols = 30;
+    @Input() rows = 10;
+    @Input('form-id') id;
+    @Input('class') className = '';
+    @Input() disabled;
+    @Input() maxlength: number;
+    @Input('value') val = '';
 
-  @Input() name = '';
-  @Input() placeholder = '';
-  @Input() cols = 30;
-  @Input() rows = 10;
-  @Input('form-id') id;
-  @Input('class') className = '';
-  @Input() disabled;
-  @Input() maxlength: number;
-  @Input('value') val = '';
-
-  get value() {
-    return this.val || '';
-  }
-
-  set value(val) {
-    this.val = val;
-    this.onChange(val);
-    this.onTouched();
-  }
-
-  get charsLeft() {
-    if (this.maxlength) {
-      return this.maxlength - this.value.length;
+    get value() {
+        return this.val || '';
     }
-    return 0;
-  }
 
-  constructor(private elementRef: ElementRef) {
-    this.elementRef.nativeElement.className = '';
-  }
+    set value(val) {
+        this.val = val;
+        this.onChange(val);
+        this.onTouched();
+    }
 
-  onChange: any = () => {
-    // Metodo inicial
-  };
-  onTouched: any = () => {
-    // Metodo inicial
-  };
+    get charsLeft() {
+        if (this.maxlength) {
+            return this.maxlength - this.value.length;
+        }
+        return 0;
+    }
 
-  writeValue(value: any) {
-    this.val = value;
-  }
+    constructor(private elementRef: ElementRef) {
+        this.elementRef.nativeElement.className = '';
+    }
 
-  registerOnChange(fn: any) {
-    this.onChange = fn;
-  }
+    onChange: any = () => {
+        // Metodo inicial
+    };
+    onTouched: any = () => {
+        // Metodo inicial
+    };
 
-  registerOnTouched(fn: any) {
-    this.onTouched = fn;
-  }
+    writeValue(value: any) {
+        this.val = value;
+    }
 
-  setDisabledState(state: boolean) {
-    this.disabled = state;
-  }
+    registerOnChange(fn: any) {
+        this.onChange = fn;
+    }
+
+    registerOnTouched(fn: any) {
+        this.onTouched = fn;
+    }
+
+    setDisabledState(state: boolean) {
+        this.disabled = state;
+    }
 }
