1c69c7e4aa4b743ba2e127dd9c71807b20480012
Ivan Florencio <ivanflorencio@hotmail.com>
2022-10-27 21:08:37 -0300

    NE-39: Bug na formalização do Projeto

--- src/app/pages/propostas/proposta/proposta.component.ts
+++ src/app/pages/propostas/proposta/proposta.component.ts
@@ -1,41 +1,32 @@
-import {Component, Inject, OnInit} from '@angular/core';
-import {Proposta, UserRole} from '@app/commons';
-import {CaptacaoIdProvider, PROPOSTA, PropostaProvider, PropostaSidebar} from './shared';
-import {BehaviorSubject} from 'rxjs';
-import {EtapasService} from '@app/users-modules/fornecedor/services/propostas.service';
-import {AuthService} from '@app/services';
-
+import { Component, Inject, OnInit } from '@angular/core';
+import { Proposta, UserRole } from '@app/commons';
+import { CaptacaoIdProvider, PROPOSTA, PropostaProvider, PropostaSidebar } from './shared';
+import { BehaviorSubject } from 'rxjs';
+import { EtapasService } from '@app/users-modules/fornecedor/services/propostas.service';
+import { AuthService } from '@app/services';
 
 @Component({
-  selector: 'app-suprimentos',
-  templateUrl: './proposta.component.html',
-  viewProviders: [PropostaProvider],
-  providers: [
-    PropostaProvider,
-    EtapasService,
-    CaptacaoIdProvider,
-    PropostaSidebar
-  ],
-
+    selector: 'app-suprimentos',
+    templateUrl: './proposta.component.html',
+    styleUrls: ['./proposta.component.scss'],
+    viewProviders: [PropostaProvider],
+    providers: [PropostaProvider, EtapasService, CaptacaoIdProvider, PropostaSidebar],
 })
 export class PropostaComponent implements OnInit {
-  proposta: Proposta;
+    proposta: Proposta;
 
-  backLink = '/';
+    backLink = '/';
 
-  constructor(@Inject(PROPOSTA) public propostaObservable: BehaviorSubject<Proposta>, protected auth: AuthService) {
+    constructor(@Inject(PROPOSTA) public propostaObservable: BehaviorSubject<Proposta>, protected auth: AuthService) {}
 
-  }
-
-  ngOnInit() {
-    this.propostaObservable.subscribe(data => {
-      this.proposta = data;
-      if (this.proposta.captacaoStatus === 'Refinamento') {
-        this.backLink = '/refinamento';
-      } else {
-        this.backLink = '/propostas';
-      }
-    });
-  }
-
+    ngOnInit() {
+        this.propostaObservable.subscribe((data) => {
+            this.proposta = data;
+            if (this.proposta.captacaoStatus === 'Refinamento') {
+                this.backLink = '/refinamento';
+            } else {
+                this.backLink = '/propostas';
+            }
+        });
+    }
 }
